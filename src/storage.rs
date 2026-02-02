33ba62b1f1eaff2676a81c4b0cd01ea03f6753d6
herabel <3423.0a@mail.ru>
2026-02-02 23:09:14 +0300

    refactor: clippy for storage.rs

--- src/storage.rs
+++ src/storage.rs
@@ -9,7 +9,7 @@ pub struct BiceFile{
 }
 
 impl BiceFile{
-    pub fn new(salt: &Vec<u8>, encrypted_data: &[u8]) -> Self {
+    pub fn new(salt: &[u8], encrypted_data: &[u8]) -> Self {
         let mut salt_array = [0u8; 64];
         salt_array.copy_from_slice(salt);
         Self {
