b3a3c1b356f91699cbfe04fc10c8a08327867783
herabel <3423.0a@mail.ru>
2026-02-05 14:56:19 +0300

    feat: impl Vault struct

--- src/models.rs
+++ src/models.rs
@@ -13,3 +13,31 @@ pub struct PasswordEntry {
 pub struct Vault {
     pub entries: Vec<PasswordEntry>,
 }
+
+impl Vault { 
+    pub fn new() -> Self {
+        Self {entries: Vec::new()}
+    }
+    pub fn add(&mut self, service: String, login: String, password: String, description: Option<String>) {
+        self.entries.push(PasswordEntry { service, login, password, description });
+    }
+
+    pub fn load_from_disk(path: &str, master_pass: &str) -> Result<Self, String> {
+        let bice = BiceFile::open("B1CE.bice").map_err(|e| e.to_string())?;
+        let decrypted_data = bice.decrypt(master_pass)?;
+        let vault: Vault = postcard::from_bytes(&decrypted_data).map_err(|e| format!("Ошибка структуры данных {e}!"))?;
+        Ok(vault)
+    }
+
+    pub fn save_to_disk(&self, path: &str, master_pass: &str, profile: crate::vault::SecurityProfile) -> Result<(), String> {
+        let bytes = postcard::to_stdvec(self).map_err(|e| e.to_string())?;
+
+        let new_salt = crate::entropy::generate_random_bytes(64);
+
+        let bice = BiceFile::encrypt_new(&bytes, master_pass, &new_salt, profile)?;
+
+        bice.save(path).map_err(|e| e.to_string())?;
+
+        Ok(())
+    }
+}
