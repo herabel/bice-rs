8fe6ad3856bf8745bdde5bd45c5686d415f8cbbc
herabel <3423.0a@mail.ru>
2026-03-15 16:31:28 +0300

    refactor: change entropy generation functions for iterations

--- src/cpu_entropy.rs
+++ src/cpu_entropy.rs
@@ -1,32 +1,35 @@
 #[cfg(any(target_arch = "x86_64"))]
 pub fn get_entropy_from_cpu() {
-    println!("{}", gen_rdseed());
-}
-
-pub fn gen_rdseed() -> u64 {
+/// Does a query to "rdseed" processor register and returns None if status == 0 or unsupported feature
+pub fn try_rdseed() -> Option<u64> {
     if is_x86_feature_detected!("rdseed") {
         unsafe {
             let mut val: u64 = 0;
             let status: i32 = std::arch::x86_64::_rdseed64_step(&mut val);
-            assert_eq!(status, 1, "RDSEED failed: hardware entropy source exhausted");
-            val
+            if status == 1 {
+                Some(val)
+            } else {
+                None
+            }
         }
     } else {
-        println!("rdseed not found");
-        0
+        None
     }
 }
 
-pub fn gen_rdrand() -> u64 {
+/// Does a query to "rdrand" processor register and returns None if status == 0 or unsupported feature
+pub fn try_rdrand() -> Option<u64> {
     if is_x86_feature_detected!("rdrand") {
         unsafe {
             let mut val: u64 = 0;
             let status: i32 = std::arch::x86_64::_rdrand64_step(&mut val);
-            assert_eq!(status, 1, "RDRAND failed: hardware entropy source exhausted");
-            val
+            if status == 1 {
+                Some(val)
+            } else {
+                None
+            }
         }
     } else {
-        println!("rdrand not found");
-        0
+        None
     }
 }
