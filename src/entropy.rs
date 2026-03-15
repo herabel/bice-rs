a3a015a3281bf9693baa01a0be76d6374d7a9a0d
herabel <3423.0a@mail.ru>
2026-03-15 16:33:21 +0300

    fix: change principle of work for new entropy gathering mechanism

--- src/entropy.rs
+++ src/entropy.rs
@@ -9,13 +9,22 @@ pub fn generate_random_bytes(size: usize) -> Vec<u8> {
     let mut hasher= Shake::v256();
 
     let mut os_buf = [0u8; 64];
-    getrandom::fill(&mut os_buf).expect("[ERROR] ОС не продоставила энтропию");
+    getrandom::fill(&mut os_buf).expect("[ERROR] : ОС не продоставила энтропию");
 
-    let hard_random_number_rdrand = cpu_entropy::gen_rdrand();
-    hasher.update(&hard_random_number_rdrand.to_le_bytes());
+    let hard_random_number_rdrand = cpu_entropy::gen_rdrand(50).unwrap_or(0);
+    if hard_random_number_rdrand != 0 {
+        hasher.update(&hard_random_number_rdrand.to_le_bytes());
+    } else {
+        println!("[WARN] : rdrand returned 0 (entropy.rs) so this source degraded");
+    }
+    
 
-    let hard_random_number_rdseed = cpu_entropy::gen_rdseed();
-    hasher.update(&hard_random_number_rdseed.to_le_bytes());
+    let hard_random_number_rdseed = cpu_entropy::gen_rdseed(50).unwrap_or(0);
+    if hard_random_number_rdseed != 0 {
+        hasher.update(&hard_random_number_rdseed.to_le_bytes());
+    } else {
+        println!("[WARN] : rdseed returned 0 (entropy.rs) so this source degraded")
+    }
 
     hasher.update(&os_buf);
 
