39cbdfb3c2a88944170bafcda8c8fb3a09734ab3
herabel <3423.0a@mail.ru>
2026-04-14 21:40:14 +0300

    chore: whitespace commit

--- src/entropy.rs
+++ src/entropy.rs
@@ -12,27 +12,27 @@ pub struct HardwareEntropyPool{
 impl HardwareEntropyPool {
     pub fn new() -> Self {
 
-    let mut hasher= Shake::v256();
+        let mut hasher= Shake::v256();
 
-    let mut os_buf = [0u8; 64];
-    getrandom::fill(&mut os_buf).expect("[ERROR] : ОС не продоставила энтропию");
+        let mut os_buf = [0u8; 64];
+        getrandom::fill(&mut os_buf).expect("[ERROR] : ОС не продоставила энтропию");
 
-    let hard_random_number_rdrand = cpu_entropy::gen_rdrand(50).unwrap_or(0);
-    if hard_random_number_rdrand != 0 {
-        hasher.update(&hard_random_number_rdrand.to_le_bytes());
-    } else {
-        println!("[WARN] : rdrand returned 0 (entropy.rs) so this source degraded");
-    }
-    
+        let hard_random_number_rdrand = cpu_entropy::gen_rdrand(50).unwrap_or(0);
+        if hard_random_number_rdrand != 0 {
+            hasher.update(&hard_random_number_rdrand.to_le_bytes());
+        } else {
+            println!("[WARN] : rdrand returned 0 (entropy.rs) so this source degraded");
+        }
+        
 
-    let hard_random_number_rdseed = cpu_entropy::gen_rdseed(50).unwrap_or(0);
-    if hard_random_number_rdseed != 0 {
-        hasher.update(&hard_random_number_rdseed.to_le_bytes());
-    } else {
-        println!("[WARN] : rdseed returned 0 (entropy.rs) so this source degraded")
-    }
+        let hard_random_number_rdseed = cpu_entropy::gen_rdseed(50).unwrap_or(0);
+        if hard_random_number_rdseed != 0 {
+            hasher.update(&hard_random_number_rdseed.to_le_bytes());
+        } else {
+            println!("[WARN] : rdseed returned 0 (entropy.rs) so this source degraded")
+        }
 
-    hasher.update(&os_buf);
+        hasher.update(&os_buf);
 
         Self { state: (hasher), counter: (0) }
     }
