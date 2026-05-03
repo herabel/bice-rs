7b549972a7cc139ba2fc1f90d2caddf94d3fcfb5
herabel <3423.0a@mail.ru>
2026-05-03 21:29:31 +0300

    feat: integrate flags for in-file settings such as esp32 necessarity

--- src/storage.rs
+++ src/storage.rs
@@ -8,7 +8,9 @@ pub struct BiceFile{
     pub header: [u8;4],
     pub version: u8,
     pub profile_id: u8,
+    pub flags: u8,
     pub salt: [u8;64],
+    pub esp32_pubkey: Option<[u8;32]>,
     pub data: Vec<u8>
 }
 
@@ -22,7 +24,9 @@ impl BiceFile{
         raw_data: &[u8], 
         password: [u8;32], 
         salt: &[u8],
-        profile: vault::SecurityProfile
+        profile: vault::SecurityProfile,
+        flags: u8,
+        esp32_pubkey: Option<[u8;32]>,
     ) -> Result<Self,String>
     {
 
@@ -40,20 +44,25 @@ impl BiceFile{
 
         Ok(Self { 
             header: (*b"B1CE"), 
-            version: (1), 
+            version: (2), 
             profile_id,
+            flags,
             salt: (salt_array), 
+            esp32_pubkey,
             data: (encrypted_bytes)
         })
     }
 
 
     pub fn get_salt_from_file(path: impl AsRef<std::path::Path>) -> std::io::Result<[u8;64]>{
-        let mut file = File::open(path)?;
-        let _ = file.seek(io::SeekFrom::Start(6));
-        let mut reader = BufReader::new(file);
+        let mut file = File::open(&path)?;
+        let mut version_buf = [0u8; 1];
+        file.seek(io::SeekFrom::Start(4))?;
+        file.read_exact(&mut version_buf)?;
+        let offset: u64 = if version_buf[0] >= 2 { 7 } else { 6 };
+        file.seek(io::SeekFrom::Start(offset))?;
         let mut salt = [0u8;64];
-        reader.read_exact(&mut salt)?;
+        file.read_exact(&mut salt)?;
         Ok(salt)
     }
 
@@ -61,7 +70,7 @@ impl BiceFile{
     /// Сохраняет текущий BiceFile по указанному пути.
     /// Логика:
     /// 1. Создать/Перезаписать файл.
-    /// 2. Последовательно записать: header -> version -> profile_id -> salt -> data.
+    /// 2. Последовательно записать: header -> version -> profile_id -> flags -> salt -> [esp32_pubkey] -> data.
     /// 3. Атомарно перезапсывает файл, создавая tmp верисию.
     /// 4. Гарантирует, что файл не будет уничтожен если вдруг компьютер выключится во время перезаписи.
     pub fn save(&self, path: impl AsRef<std::path::Path>) -> std::io::Result<()> {
@@ -77,7 +86,11 @@ impl BiceFile{
             file.write_all(&self.header)?;
             file.write_all(&[self.version])?;
             file.write_all(&[self.profile_id])?;
+            file.write_all(&[self.flags])?;
             file.write_all(&self.salt)?;
+            if let Some(ref pubkey) = self.esp32_pubkey {
+                file.write_all(pubkey)?;
+            }
             file.write_all(&self.data)?;
             file.sync_all()?;
         }
@@ -113,8 +126,23 @@ impl BiceFile{
             return Err(io::Error::new(io::ErrorKind::InvalidData, "[FS] : Неизвестный профиль безопасности"));
         }
 
+        let mut flags = 0u8;
+        if version >= 2 {
+            let mut flags_buf = [0u8; 1];
+            reader.read_exact(&mut flags_buf)?;
+            flags = flags_buf[0];
+        }
+
         let mut salt = [0u8;64];
         reader.read_exact(&mut salt)?;
+
+        let mut esp32_pubkey = None;
+        if flags & 1 != 0 {
+            let mut pubkey = [0u8; 32];
+            reader.read_exact(&mut pubkey)?;
+            esp32_pubkey = Some(pubkey);
+        }
+
         let mut data = Vec::new();
         reader.read_to_end(&mut data)?;
 
@@ -122,7 +150,9 @@ impl BiceFile{
             header,
             version,
             profile_id,
+            flags,
             salt,
+            esp32_pubkey,
             data
         })
     }
@@ -141,5 +171,24 @@ impl BiceFile{
         let mut profile_buf = [0u8;1];
         reader.read_exact(&mut profile_buf).ok()?;
         Some(profile_buf[0])
+    }
+
+    pub fn get_flags(path: impl AsRef<std::path::Path>) -> Option<u8> {
+        let file = File::open(&path).ok()?;
+        let mut reader = BufReader::new(file);
+        let mut version_buf = [0u8; 1];
+        reader.seek_relative(4).ok()?;
+        reader.read_exact(&mut version_buf).ok()?;
+        if version_buf[0] < 2 {
+            return Some(0);
+        }
+        reader.seek_relative(1).ok()?;
+        let mut flags_buf = [0u8; 1];
+        reader.read_exact(&mut flags_buf).ok()?;
+        Some(flags_buf[0])
+    }
+
+    pub fn requires_esp32(&self) -> bool {
+        self.flags & 1 != 0
     }
 }
